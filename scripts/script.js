51b746c8f3d5d64111da8aedacd077c42fc8356f
roos-hva <90178913+roos-hva@users.noreply.github.com>
2021-09-28 13:06:43 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 // JavaScript Document
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", openMenu);
+
+function openMenu() {
+  document.body.classList.toggle("menuOpen");
+}
